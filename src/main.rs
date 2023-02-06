dddfc6e82a8786337720ca561174e8d9c8598355
Filipe Santos <filipesantos1999@hotmail.com>
2023-02-06 20:31:24 +0000

    Add read file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 // internal
 
 use std::env::args;
+use std::fs::read_to_string;
 
 
 fn main() {
     let args: Vec<String> = get_args();
+    println!("\nArgs: {:?}", args);
 
-    println!("{:?}", args);
+    let file = read_file(&args[1]);
+    println!("\nFile content: {:?}", file);
 }
-
 
 fn get_args() -> Vec<String> {
     return args().collect();
+}
+
+fn read_file (path: &String) -> String {
+    let file_name = path.to_owned() + ".txt";
+    return read_to_string(file_name).expect("Read File Error");
 }
